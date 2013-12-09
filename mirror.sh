326ccd7d3ad4c2ebcb3222444a7a914727d8011d
Michael Farrell <micolous+git@gmail.com>
2013-12-10 01:21:32 +1030

    Updated mirror script to include release notes in commit message.

--- mirror.sh
+++ mirror.sh
@@ -13,8 +13,33 @@
 DATA_ZIP="http://adelaidemetro.com.au/GTFS/google_transit.zip"
 DATA_ZIPFILENAME="google_transit.zip"
 
+function get_release_isodate {
+  # Gets the ISO8601 date of the release
+  D=`grep "Release: " "$1" | head -n1 | cut -d" " -f2 | sed 's#-#/#g'`
+  if [ -n "$D" ]; then
+    Y=`echo $D | cut -d/ -f3`
+    M=`echo $D | cut -d/ -f2`
+    A=`echo $D | cut -d/ -f1`
+
+    echo "${Y}-${M}-${A}:${1}"
+  else
+    # Cannot parse
+    echo "0000-00-00:${1}"
+  fi
+}
+
+function get_release_dates {
+  find gtfs/ -name 'release\ notes*.txt' | while read i
+  do
+    get_release_isodate "$i"
+  done
+}
+
 # Create the target folder
 mkdir -p "gtfs"
+
+# Delete old release notes from the folder
+rm -f 'gtfs/release notes*.txt'
 
 # Download data archive
 # Normally -O would be used here, but it doesn't work in combination with -N.
@@ -29,7 +54,13 @@ git add gtfs/*.txt
 
 # Commit
 DATE="`date`"
-git commit -m "New data from upstream source on ${DATE}"
+HEADER="New data from upstream source on ${DATE}\n"
+
+# Find the newest release notes and construct a commit message from them
+NEW_NOTES=`get_release_dates | sort -r | head -n1 | cut -d: -f2`
+MSG=`echo -e $HEADER | cat - "${NEW_NOTES}"`
+
+git commit -am "${MSG}"
 
 # Push downstream to github
 git push origin master
